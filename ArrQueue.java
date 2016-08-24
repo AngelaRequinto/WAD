e9bcdacafd97901953e91e7fd043a786d201e9f1
AngelaRequinto <IamMinako22@gmail.com>
2016-08-24 17:47:38 +0800

    Update ArrQueue.java

--- ArrQueue.java
+++ ArrQueue.java
@@ -3,137 +3,123 @@
  Angela Mikaela R. Requinto
  Bsis-2
  Github.com/AngelaRequinto
- August 23,2016
- 
+ august 23,,2016
+
  */
 package arrqueue;
 
 public class ArrQueue {
 
-    private int QSIZE;
-    private int[] queue;
-    private int nItems;
+    private int capacity;
+    private int nItems=0;
+    private String[]storage;
     
     public ArrQueue(int size){
-        QSIZE = size;
-        queue = new int[QSIZE];
-    }
 
-    public void showQueue(){
-        int i;
-        for(i = 0; i < QSIZE; i++){
-            System.out.printf("Queue[%d] = %d\n", i, queue[i]);
-        }   
+    capacity = size;
+    storage=new String[capacity];
     }
-
-    private boolean isEmpty(){
-        if(nItems == 0){
-        System.out.println("QUEUE is EMPTY");
+    
+    public void show(){
+    
+        for (int i=0; i<=capacity-1; i++){
+        System.out.println("Queue["+i+"] "+storage[i]);
         
-        return true;
-        
-        } else {
-            
-        return false;
-        }
     }
-
-    private boolean isFull(){
-        if(nItems == QSIZE){
-            System.out.println("Queue is FULL");
-            return true;
-        } else {
-            
-        return false;
-        }
+        System.out.println();
     }
-
-    public boolean enqueue(int num){
-        System.out.print("Trying to enqueue " + num + "... ");
-        if(! isFull()){
-            queue[nItems] = num;
-            System.out.println("Added " + num);
-            nItems++;
-            return true;
-        } else {
-            System.out.println("Failed to enqueue " + num);
-            return false;
-        }
-    }
-
-    public void dequeue(){
-        System.out.print("Dequeuing... ");
-        if(! isEmpty()){
-            int num = queue[0];
-            System.out.println("Removed " + num);
-            rearrangeQueue();
-        } else {
-            System.out.println("Failed to dequeue");
-        }
-    }
-
-    private void rearrangeQueue(){
-        System.out.println("Rearranging Queue");
-        int[] temp = new int[nItems];
-        int i;
-        for(i = 0; i < (nItems - 1); i++){
-            temp[i] = queue[i + 1];
-        }
-        nItems--;
-        for(i = 0; i < QSIZE; i++){
-            if(i <= nItems){
-            queue[i] = temp[i];
-        } else {
-            queue[i] = 0;
-            }
-        }
-    }
-
-    public int peekFront(){
-        return queue[0];
+    
+    
+    public boolean isEmpty(){
+     if (nItems==0) {
+      System.out.println("Queue is empty");
+      System.out.println();
+      return true;
     } 
-
-    public int peekRear(){
-        return queue[nItems];
+    return false;
     }
-}
+   
+    private boolean isFull() {
+    if (nItems == capacity) {
+        System.out.println("Stack is Full");
+      return true;
+    } else{
+    return false;
+    }
+   }
     
-
+    public void enqueue(String value){
+       
+       System.out.println("Trying to enqueue number"+value+ "... "); 
+       if (isFull()){
+           System.out.println("Added failed");
+           
+        }
+       else{
+           storage[nItems]= value;
+           nItems++;
+           System.out.println(value+" Added");
+           
+       }
+    }
+    public void dequeue(){
     
+        System.out.print("Trying to dequeue...");
+        if( isEmpty()){
+        System.out.println("Queue is Empty");
+       } else{
+            storage[nItems]=null;
+            nItems--;
+            System.out.println("Storage ["+nItems+"] removed");
+           
+          }
+      }
     
+    public void peekfront(){
+    System.out.println("Head = "+storage[nItems]);
+    }
+    public void peekrear(){
+    System.out.println("Tail = "+storage[0]);
+    }
     
     
     public static void main(String[] args) {
+        
+        ArrQueue ArrQueue= new ArrQueue (10);
+        
+        System.out.println("STORAGE CAPACITY:  " +ArrQueue.capacity );
+        System.out.println();
+        
+        ArrQueue.show();
+        ArrQueue.isEmpty();
+        ArrQueue.enqueue("one");
+        ArrQueue.show();
+        ArrQueue.show();
+        ArrQueue.dequeue();
+        ArrQueue.enqueue("two");
+        ArrQueue.dequeue();
+        ArrQueue.enqueue("three");
+        ArrQueue.show();
+        ArrQueue.enqueue("four");
+        ArrQueue.peekfront();
+        ArrQueue.peekrear();
+        ArrQueue.enqueue("five");
+        ArrQueue.dequeue();
+        ArrQueue.enqueue("six");
+        ArrQueue.enqueue("seven");
+        ArrQueue.show();
+        ArrQueue.dequeue();
+        ArrQueue.enqueue("eight");
+        ArrQueue.enqueue("nine");
+        ArrQueue.enqueue("ten");
+        ArrQueue.show();
+        ArrQueue.isFull();
+        ArrQueue.peekfront();
+        ArrQueue.peekrear();
+  
       
-       ArrQueue ArrQueue= new ArrQueue (10);
-       System.out.println("Storage Capacity:"+ArrQueue.capacity);
-       System.out.print();
-       
-       ArrQueue.show();
-       ArrQueue.isEmpty();
-       ArrQueue.enqueue("one");
-       ArrQueue.show();
-       ArrQueue.show();
-       ArrQueue.dequeue();
-       ArrQueue.enqueue("two");
-       ArrQueue.dequeue();
-       ArrQueue.enqueue("three");
-       ArrQueue.show();
-       ArrQueue.enqueue("four");
-       ArrQueue.peekfront();
-       ArrQueue.peekrear();
-       ArrQueue.enqueue("five");
-       ArrQueue.dequeue();
-       ArrQueue.enqueue("six");
-       ArrQueue.enqueue("seven");
-       ArrQueue.show();
-       ArrQueue.dequeue();
-       ArrQueue.enqueue("eight");
-       ArrQueue.enqueue("nine");
-       ArrQueue.enqueue("ten");
-       ArrQueue.isFull();
-       ArrQueue.peekfront();
-       ArrQueue.peekrear();
+         }
+
+  
     }
     
-}
